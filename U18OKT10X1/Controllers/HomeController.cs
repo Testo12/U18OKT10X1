7cef0ad5f8394351b1ac39656b47a0498b52ce30
Testo12 <erik.malmsten99@gmail.com>
2018-10-10 11:42:10 +0200

    Created framework, added site views, basic modles.

---
+++ U18OKT10X1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace U18OKT10X1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Cars()
+        {
+            return View();
+        }
+
+        public ActionResult Sucess()
+        {
+            return View();
+        }
+    }
+}
