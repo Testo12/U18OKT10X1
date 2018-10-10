c13ed850b818bf77b3fc2194e3bb90646bd077b8
Testo12 <erik.malmsten99@gmail.com>
2018-10-10 12:22:39 +0200

    Added register from.

---
+++ U18OKT10X1/Models/RegisterInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace U18OKT10X1.Models
+{
+    public class RegisterInfo
+    {
+        [Required(ErrorMessage = "Please Eneter Your Name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Please Eneter Your Email")]
+        public String Email { get; set; }
+
+        [Required(ErrorMessage = "Please Eneter Your Phone number")]
+        public String Phone { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Adress")]
+        public bool Adress { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Driver licens information")]
+        public bool Licens { get; set; }
+
+    }
+}
